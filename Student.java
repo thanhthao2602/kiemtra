f5b00556b1b23539ac4fad36b134c2e26f40e3ee
thanhthao2602 <140616862+thanhthao2602@users.noreply.github.com>
2023-09-13 10:25:13 +0700

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,12 +1,45 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+import java.util.Scanner;
+public class Student extends Person {
+  private int id ;
+  private Address address;
+  private double gpa; 
 
-/**
- *
- * @author 84363
- */
-public class Student {
-    
+    public Student() {
+    }
+
+    public Student(int id, Address address, double gpa) {
+        this.id = id;
+        this.address = address;
+        this.gpa = gpa;
+    }
+
+    public Student(int id, Address address, double gpa, String name, int age, String gender) {
+        super();
+        this.id = id;
+        this.address = address;
+        this.gpa = gpa;
+    }
+    public void input(){
+        
+        super.input();
+          Scanner s = new Scanner(System.in);
+        System.out.println("Nhap GPA : ");
+        gpa = s.nextDouble();
+          System.out.println("Nhap dia chi : "); 
+        address = new Address();
+        address.input();
+        System.out.println("Nhap id : ");
+        id = s.nextInt();
+    }
+    public void output(){
+        super.output();
+        System.out.println("ID la : "+id);
+         
+          System.out.println("GPA la : "+gpa);
+           
+          
+    }
 }
+ 
+
+
