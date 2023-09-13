8ff1bfa52447025c99e92e41ea8deae9eb674ff8
thanhthao2602 <140616862+thanhthao2602@users.noreply.github.com>
2023-09-13 10:22:55 +0700

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,12 +1,53 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author 84363
- */
+import java.util.Scanner;
 public class Person {
-    
+  protected String name;
+ protected int age ;
+ protected String gender;
+
+    public Person() {
+    }
+
+    public Person(String name, int age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    public void input(){
+       Scanner s = new Scanner(System.in);
+       System.out.println("Nhap ten : ");
+       name = s.nextLine();
+       System.out.println("Nhap tuoi : ");
+       age = s.nextInt();
+       System.out.println("Nhap gioi tinh : ");
+       gender = s.nextLine();
+    }
+    public void output(){
+        System.out.println("Ten la : "+name);
+        System.out.println("Tuoi la : "+age);
+        System.out.println("Gioi tinh la : "+gender);
+    }   
 }
